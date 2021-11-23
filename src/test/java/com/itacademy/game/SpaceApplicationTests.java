58f5c591e74ad213a61c09d2f04ca1a4eeb69c78
Alexander <89515014507@mail.ru>
2021-11-24 02:13:23 +0600

    star my project works!!

---
+++ src/test/java/com/itacademy/game/SpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itacademy.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
